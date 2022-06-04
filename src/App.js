0813987a8f93b50cf8d0f54e2e4040a4140c3f48
Tayfur Mustafa DÖNMEZ <mltm0204@gmail.com>
2022-06-04 16:51:11 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Lets get started</h1>
-    </div>
+    <Fragment>
+      <Header/>
+    </Fragment>
   );
 }
 
