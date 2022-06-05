ccfe24cb31b2671f03c46c4d27cdbbf47166cccf
Tayfur Mustafa DÖNMEZ <mltm0204@gmail.com>
2022-06-05 06:06:53 +0300

    Available meal, mealıtem, meal ıtem form add to cart

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './MealItemForm.module.css'
+
+const MealItemForm = () => {
+  return (
+    <div>MealItemForm</div>
+  )
+}
+
+export default MealItemForm
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import MealsSummary from './MealsSummary'
+import AvailableMeals from './AvailableMeals'
 
 const Meals = () => {
   return (
-    <div>Meals</div>
+    <Fragment>
+        <MealsSummary/>
+        <AvailableMeals/>
+    </Fragment>
   )
 }
 
