7356f9a389b0e849762a0d97dd1e13b6c2c4a7ed
Tayfur Mustafa DÖNMEZ <mltm0204@gmail.com>
2022-06-09 11:52:29 +0300

    handling errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -33,10 +33,16 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [httpError, setHttpError] = useState()
 
   useEffect(()=>{
     const fetchMeals = async () => {
     const response = await fetch('https://food-order-app-1892e-default-rtdb.firebaseio.com/meals.json');
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!')
+    }
+
     const responseData = await response.json();
 
     const loadedMeals = [];
@@ -52,13 +58,24 @@ const AvailableMeals = () => {
     setMeals(loadedMeals)
     setIsLoading(false)
     };
-    fetchMeals();
+    
+    fetchMeals().catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+    });
   },[])
 
   if (isLoading) {
     return (
       <section>
         <p className={classes.MealsLoading}>Loading...</p>
+      </section>
+    )
+  }
+  if (httpError) {
+    return (
+      <section>
+        <p className={classes.MealsError}>{httpError}</p>
       </section>
     )
   }
